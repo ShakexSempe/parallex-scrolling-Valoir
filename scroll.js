57c92cde1e46b1ff0e4a23752dd553bd8adbbd5f
Ray-Shakex <bshakex@gmail.com>
2021-05-23 20:32:17 +0200

    box shadows and body-middle:after, outline

--- scroll.js
+++ scroll.js
@@ -1,17 +1,17 @@
 const box = document.getElementById('box');
+const container = document.querySelector('cube-container');
 
-const scroll = () => {
+scroll = () => {
     window.addEventListener('scroll', function () {
         var value = window.scrollY;
         // btn.style.left = value * 0.1 + 'px';
         btn.style.top = value * 0.14 + 'px';
-        console.log(value)
 });
 }
 
 
 const observer =  new IntersectionObserver(scroll, {
-    threshold: [.5, .8]
+    threshold: [.5]
 });
 
 
