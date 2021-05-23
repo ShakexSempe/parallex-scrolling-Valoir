5781f6688f1e8f7598c790af1cf1b5bce820cd36
Ray-Shakex <bshakex@gmail.com>
2021-05-23 12:28:39 +0200

    edit intersectionObserver and removed ../ from script tag

--- scroll.js
+++ scroll.js
@@ -1,20 +1,17 @@
-let btn = document.getElementById("btn");
-
-
-
 const box = document.getElementById('box');
-
 const scroll = () => {
     window.addEventListener('scroll', function () {
         var value = window.scrollY;
         // btn.style.left = value * 0.1 + 'px';
-        btn.style.top = value * 0.3 + 'px';
+        btn.style.top = value * 0.16 + 'px';
         console.log(value)
 });
 }
 
+
 const observer =  new IntersectionObserver(scroll, {
-    threshold: [.3, .4, .8]
+    threshold: [.5, .8]
 });
+
 
 observer.observe(box);
